f4ec347d360910eeec200ab73235ab501449e093
Cairo <caio12013marcelo@gmail.com>
2026-06-18 19:06:34 -0300

    Update ex03.java

--- Ex/src/ex03.java
+++ Ex/src/ex03.java
@@ -1,11 +1,17 @@
+import java.util.Scanner;
+
 public class ex03 {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         int[] vetor = new int[5];
 
         for (int i = 0; i < vetor.length; i++) {
-            for (int j = 0; j < vetor.length; j++) {
-                System.out.println();
-            }
+            System.out.println("Informe um valor: ");
+            vetor[i]= input.nextInt();
         }
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.println(vetor[i]);
+        }
+        input.close();
     }
 }
