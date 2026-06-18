31e679de5d169ab0d3540f593970ec925540c1f1
Cairo <caio12013marcelo@gmail.com>
2026-06-18 19:01:35 -0300

    asd

---
+++ Ex/src/ex02.java
@@ -0,0 +1,8 @@
+public class ex02 {
+    public static void main(String[] args) {
+        int[] v = {5, 3, 8, 1, 9, 4, 7};
+        System.out.printf("Primeiro : "+ v[0]);
+        System.out.printf("Meio : "+ v[v.length /2]);
+        System.out.printf("Ultimo : "+ v[v.length -1] );
+    }
+}
---
+++ Ex/src/ex03.java
@@ -0,0 +1,11 @@
+public class ex03 {
+    public static void main(String[] args) {
+        int[] vetor = new int[5];
+
+        for (int i = 0; i < vetor.length; i++) {
+            for (int j = 0; j < vetor.length; j++) {
+                System.out.println();
+            }
+        }
+    }
+}
